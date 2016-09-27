9aad28a3467bdbfaf9a6f529af571bf28bc7ff6b
Totoro le Pacha <totorolepacha@gmail.com>
2016-09-28 00:43:07 +0200

    Création et mise en place d'une interface graphique

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # trajectoire-de-bulle
 Projet visant à déterminer les trajectoires de bulles à partir d'un fichier de données
+
+## Packages utilisés
+pip8 nécessaire
+
+### via pip
+matplotlib
+
+### via apt-get
+python3-tk
--- gui/interface.py
+++ gui/interface.py
@@ -1,10 +1,66 @@
 import pylab as p
 import mpl_toolkits.mplot3d.axes3d as p3
 
-fig=p.figure()
-ax = fig.add_subplot(111, projection='3d')
 
-ax.scatter([1], [0], [0], c='r', marker='^', picker=5)
-ax.scatter([0], [1], [0], c='g', marker='^', picker=5)
-ax.scatter([0], [0], [1], c='b', marker='^', picker=5)
 
+class GUI:
+
+    def __init__(self, bubbles, trajectories):
+        """
+        Méthode instanciant un affichage pour tous les points et trajectoires passés en paramètre.
+
+        :param bubbles: Liste de toutes les coordonnées de bulles sous forme de tuples (x, y, z).
+        :param trajectories: Liste des tuples contenant les 5 tuples de coordonnées des points à atteindre.
+        """
+        self.bubbles = bubbles
+        self.trajectories = trajectories
+
+    def display(self):
+        """
+        Méthode affichant les points restés en mémoire.
+
+        """
+
+        fig = p.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        for bubble in self.bubbles:
+            ax.scatter([bubble[0]], [bubble[1]], [bubble[2]], c='b', marker='o', picker=5)
+
+        self.trajectories = self.trajectories
+
+        fig.canvas.mpl_connect('pick_event', self.onpick)
+
+        p.show()
+
+    def onpick(self, event):
+        ind = event.ind[0]
+        x, y, z = event.artist._offsets3d
+
+        trajectories = [coordinates for coordinates in self.trajectories if (x[ind], y[ind], z[ind]) in coordinates]
+
+        if len(trajectories) > 0:
+            for i in range(4):
+                ax.plot([trajectories[0][i][0], trajectories[0][i+1][0]], [trajectories[0][i][1], trajectories[0][i+1][1]], zs=[trajectories[0][i][2], trajectories[0][i+1][2]])
+
+        print(x[ind], y[ind], z[ind])
+
+
+if __name__ == '__main__':
+    fig = p.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.scatter([1], [0], [0], c='r', marker='^', picker=5)
+    ax.scatter([0], [1], [0], c='g', marker='^', picker=5)
+    ax.scatter([0], [0], [1], c='b', marker='^', picker=5)
+
+    ax.plot([1, 0], [0, 1], zs=[0, 0])
+
+    def onpick(event):
+        ind = event.ind[0]
+        x, y, z = event.artist._offsets3d
+        print(x[ind], y[ind], z[ind])
+
+    fig.canvas.mpl_connect('pick_event', onpick)
+
+    p.show()
