635e70a82f8e3b9037c097298d81382b1e2db193
merlin <merlingoulet@live.fr>
2016-09-30 14:42:34 +0200

    Implémentation fonctionnel du réseau de neurone. Manque un ensemble de données négative

---
+++ reseau_neuronal/network.py
@@ -0,0 +1,67 @@
+from preprocessing.candidates      import CandidateSearch
+from preprocessing.datamodel       import LearningSet, PointSet
+from reseau_neuronal.reseau_neural import NeuralNetwork
+
+import math
+import random
+
+
+class Network:
+    def __init__(self, train_sets: list = None, search_set: PointSet = None):
+        self.search  = search_set
+
+        if train_sets is None:
+            self.network = NeuralNetwork()
+        else:
+            #On tranforme la liste de LearningSet en matrice unique
+            matrice_input_vrais = []
+            for train_set in train_sets:
+                for candidat in train_set.iterate():
+                    row = [coordonnee for point in candidat.points
+                           for coordonnee in (point.x, point.y, point.z)]
+                    matrice_input_vrais.append(row)
+
+            max_input = max([max(l) for l in matrice_input_vrais])
+            min_input = min([min(l) for l in matrice_input_vrais])
+
+            #On crée la matrice des sorties attendus en mettant toute les réponses a Vrais
+            matrice_output_vrais = [NeuralNetwork.vrais] * len(matrice_input_vrais)
+
+            #On ajoute un ensemble de candidat que l'on estime faux
+            matrice_input_faux = [[random.uniform(min_input, max_input) for _ in range(NeuralNetwork.nbNeuroneEntree)] for _ in range(len(matrice_input_vrais))]
+
+            #On crée une autre matrice de réponse attendus en mettant les réponse à faux
+            matrice_output_faux = [NeuralNetwork.faux] * len(matrice_input_faux)
+
+            #On crée la matrice complète
+            matrice_input  = matrice_input_vrais + matrice_input_faux
+            matrice_output = matrice_output_vrais + matrice_output_faux
+
+            #On mélange de la même façon les deux matrices
+            seed = random.random()
+            random.shuffle(matrice_input, lambda: seed)
+            random.shuffle(matrice_output, lambda: seed)
+
+            #On créer enfin le réseau
+            self.network = NeuralNetwork(matrice_input, matrice_output)
+
+    def perform_all_verification(self, search_set: PointSet = None):
+        if search_set is not None:
+            self.search = search_set
+
+        if self.search is None:
+            raise ValueError('Aucun ensemble sur lequel rechercher')
+        else:
+            search = CandidateSearch(self.search)
+            for candidat in search.iterate():
+                vector = [coordonnee for point in candidat.points
+                            for coordonnee in (point.x, point.y, point.z)]
+                output = self.network.use_reseau(vector)
+                reponse = [math.ceil(out) for out in output[0]]
+                if reponse == NeuralNetwork.vrais:
+                    print("Vrais")
+                    search.report_positive(candidat)
+                elif reponse == NeuralNetwork.faux:
+                    print("Faux")
+                else:
+                    print("Bug")
--- reseau_neuronal/reseau_neural.py
+++ reseau_neuronal/reseau_neural.py
@@ -3,24 +3,28 @@ import neurolab as nl
 
 class NeuralNetwork:
 
-    def __init__(self, matrice_input: list = None, vector_output: list = None):
+    vrais = [1, 0]
+    faux  = [0, 1]
+    nbNeuroneEntree = 15
+
+    def __init__(self, matrice_input: list = None, matrice_output: list = None):
         """
         Ce constructeur entrainera le réseau en plus de le crée. Ne l'utilisez que si vous êtes sur
         que vosu ne possèdez pas déjà un réseau dans un fichier.
         :param matrice_input: Matrice dont chaque ligne est une trajectoire à tester
-        :param vector_output: Vecteur dont chaque valeur est une sortie attendus
+        :param matrice_output: Vecteur dont chaque valeur est une sortie attendus
         """
 
-        if matrice_input == None and vector_output == None:
+        if matrice_input == None and matrice_output == None:
             self.network = nl.load('network.net')
 
-        elif matrice_input != None and vector_output != None:
-            self.init_reseau(matrice_input, vector_output)
+        elif matrice_input != None and matrice_output != None:
+            self.init_reseau(matrice_input, matrice_output)
 
         else:
-            raise ValueError('Vous ne pouvez pas passer un vecteur null et uen matrice non-null et vice-versa')
+            raise ValueError('Il est impossible qu\'une matrice soit null et l\'autre non')
 
-    def init_reseau(self, matrice_input: list, vector_output: list):
+    def init_reseau(self, matrice_input: list, matrice_output: list):
         """
         Méthode initiant le réseau de neurone à partir d'un
         pool de base. Il faut passer une matrice ou chaque ligne est
@@ -30,15 +34,19 @@ class NeuralNetwork:
 
         Le réseau est sauvegardé dans le fichier network.net
         :param matrice_input: Matrice dont chaque ligne est une trajectoire à tester
-        :param vector_output: Vecteur dont chaque valeur est une sortie attendus
+        :param matrice_output: Vecteur dont chaque valeur est une sortie attendus
         pour le réseau pour la ligne correspondante
         :return: Rien
         """
         max_input=max([max(l) for l in matrice_input])
         min_input=min([min(l) for l in matrice_input])
 
-        net = nl.net.newff([[min_input, max_input]*15], [100, 2])
-        net.train(matrice_input, vector_output)
+        mat_neurone_entree = [[min_input, max_input]]*15
+        nb_neurone_cachee  = 10
+        nb_neurone_sortie  = 2
+
+        net = nl.net.newff(mat_neurone_entree, [nb_neurone_cachee, nb_neurone_sortie])
+        net.train(matrice_input, matrice_output, epochs=3600)
         net.save('network.net')
 
         self.network = net
@@ -49,4 +57,4 @@ class NeuralNetwork:
         trajectoire. On cherche à tester si c'est bien le cas.
         :return: La valeur de retour du réseau
         """
-        return self.network.sim(vector_groupeToTest)
+        return self.network.sim([vector_groupeToTest])
