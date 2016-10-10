411f6655441b81bc27444ef4529c1afb9947c286
Totoro le Pacha <totorolepacha@gmail.com>
2016-10-10 17:57:59 +0200

    Sélection d'un fichier opérationnelle

--- gui/launcher.py
+++ gui/launcher.py
@@ -1,26 +1,42 @@
 import tkinter as tk
+import os
 from tkinter import *
+from tkinter.filedialog import askopenfilename
+from tkinter.messagebox import showerror
+from tkinter.messagebox import showinfo
 
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
-        #self.pack()
         self.create_widgets(master)
 
     def create_widgets(self, master):
         Label(master, text="Trajectoires de bulles", font=("fixedsys", 17))\
             .grid(row=0, sticky=W+N+E, columnspan=2, padx=15, pady=15)
-        self.load = Button(master, text="Parcourir", command=self.load_file)\
-            .grid(row=1, column=0, sticky=W)
-        self.loaded_file = Label(master, text="Aucun fichier chargé.")\
-            .grid(row=1, column=1, sticky=W+E)
+
+        self.load = Button(master, text="Parcourir", command=self.load_file)
+        self.load.grid(row=1, column=0, sticky=W+E)
+
+        self.loaded_file = Label(master, text="Aucun fichier chargé.")
+        self.loaded_file.grid(row=1, column=1)
 
     def load_file(self):
-        print("Bonjour")
+        fname = askopenfilename(filetypes=(("Fichiers de résultats", "*.txt"),
+                                           ("All files", "*.*")))
+        if fname:
+            try:
+                print("Fichier ouvert : " + fname)
 
-    def say_hi(self):
-        print("hi there, everyone!")
+                # TODO: Charger le fichier, trouver les trajectoires, et afficher le plot correspondant
+
+                self.loaded_file.config(text="Fichier chargé : {}".format(os.path.basename(str(fname))))
+                showinfo("Ouverture de fichier", "Fichier chargé avec succès !")
+            except Exception as e:  # <- naked except is a bad idea
+                print(e)
+                showerror("Ouverture de fichier", "Erreur lors de l'ouverture du fichier\n{} : {}".format(fname, e))
+            return
+
 
 root = tk.Tk()
 app = Application(master=root)
